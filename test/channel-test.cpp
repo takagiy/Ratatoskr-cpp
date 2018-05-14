33c297b52699f59fffc4974a4b09a4b369f1bf86
GobanTKG <yoshi.minase@gmail.com>
2018-05-15 00:04:30 +0900

    logでthread idを表示するようにする

--- test/channel-test.cpp
+++ test/channel-test.cpp
@@ -8,21 +8,22 @@ int main() {
   using namespace ratatoskr::concurrent;
   using namespace std::chrono_literals;
 
-  auto [sn, rc] = make_channel<int>();
-
   auto log = [](auto tag, auto x) {
     static std::mutex io_mutex;
     std::lock_guard lock{io_mutex};
-    std::cout << tag << ": " << x << std::endl;
+    std::cout << tag << ": " << x << " @thread #" << std::this_thread::get_id()
+              << std::endl;
   };
+
+  auto [sn, rc] = make_channel<int>();
 
   auto produce = [&log](auto sn) {
     for (int i = 0; i < 10; ++i) {
-      log("send", i);
+      log("send   ", i);
       sn.push(i);
       std::this_thread::sleep_for(1s);
     }
-    log("send", "close");
+    log("send   ", "close");
     sn.close();
   };
 
