f0638f85f3af20c84e47de86ec2e992225d084a0
GobanTKG <yoshi.minase@gmail.com>
2018-08-14 00:25:24 +0900

    add more test for bundle::bundle_with

--- test/functional-test.cpp
+++ test/functional-test.cpp
@@ -17,17 +17,19 @@ int main() {
           .then([](auto n) { cout << n; }),
       [](...) { cout << " "; }};
 
-  auto fizz_buzz_ = rat::bundle{}.bundle_with(
-      rat::thunk{}.filter([](auto n) { return n % 3 == 0; }).then([](...) {
-        cout << "Fizz";
-      }),
-      rat::thunk{}.filter([](auto n) { return n % 5 == 0; }).then([](...) {
-        cout << "Buzz";
-      }),
-      rat::thunk{}
-          .filter([](auto n) { return n % 3 != 0 && n % 5 != 0; })
-          .then([](auto n) { cout << n; }),
-      [](...) { cout << endl; });
+  auto fizz_buzz_ =
+      rat::bundle{
+          rat::thunk{}.filter([](auto n) { return n % 3 == 0; }).then([](...) {
+            cout << "Fizz";
+          }),
+          rat::thunk{}.filter([](auto n) { return n % 5 == 0; }).then([](...) {
+            cout << "Buzz";
+          })}
+          .bundle_with(
+              rat::thunk{}
+                  .filter([](auto n) { return n % 3 != 0 && n % 5 != 0; })
+                  .then([](auto n) { cout << n; }),
+              [](...) { cout << endl; });
 
   for (size_t i = 1; i <= 20; ++i) {
     fizz_buzz(i);
