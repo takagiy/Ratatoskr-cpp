c6f3480c859961b9aa0ffa1f78ac7a44020fdf15
GobanTKG <yoshi.minase@gmail.com>
2018-08-13 23:48:41 +0900

    test for rat::functional

---
+++ test/functional-test.cpp
@@ -0,0 +1,23 @@
+#include "../ratatoskr/functional.hpp"
+#include <cstddef>
+#include <iostream>
+
+int main() {
+  using namespace std;
+
+  auto fizz_buzz = rat::bundle{
+      rat::thunk{}.filter([](auto n) { return n % 3 == 0; }).then([](...) {
+        cout << "Fizz";
+      }),
+      rat::thunk{}.filter([](auto n) { return n % 5 == 0; }).then([](...) {
+        cout << "Buzz";
+      }),
+      rat::thunk{}
+          .filter([](auto n) { return n % 3 != 0 && n % 5 != 0; })
+          .then([](auto n) { cout << n; }),
+      [](...) { cout << endl; }};
+
+  for (size_t i = 1; i <= 20; ++i) {
+    fizz_buzz(i);
+  }
+}
