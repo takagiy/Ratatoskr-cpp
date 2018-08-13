3d2cebd00a948f40a4ae733fce05b0d3e0200a44
GobanTKG <yoshi.minase@gmail.com>
2018-08-14 07:20:43 +0900

    new test for signal

--- test/signal-test.cpp
+++ test/signal-test.cpp
@@ -1,41 +1,44 @@
+#include "../ratatoskr/reactive.hpp"
+#include <chrono>
+#include <cstddef>
 #include <iostream>
 #include <mutex>
-#include <ratatoskr/reactive.hpp>
 #include <thread>
 
-int main() {
+auto main() -> int {
   using namespace std::chrono_literals;
-  using std::this_thread::sleep_for;
-
   auto log = [](auto tag, auto data) {
     static std::mutex m;
+
     std::lock_guard lock{m};
     std::cout << tag << ": " << data << " @" << std::this_thread::get_id()
               << std::endl;
   };
 
   rat::scheduler sch;
-  rat::source<int> odd;
-  rat::source<int> even;
+  rat::channel<int> odd;
+  rat::channel<int> even;
 
-  odd.filter([](int n) { return n % 2 == 1; })
-      .then([&log](int n) { log("odd ", n); })
-      .finally([&log] { log("odd ", "end"); })
+  rat::signal{odd}
+      .filter([](int n) { return n % 2 == 1; })
+      .then([&log](int n) { log("receive(odd) ", n); })
+      .finally([&log] { log("receive(odd) ", "close"); })
       .run_on(sch, 4);
 
-  even.filter([](int n) { return n % 2 == 0; })
-      .then([&log](int n) { log("even", n); })
-      .finally([&log] { log("even", "end"); })
+  rat::signal{even}
+      .filter([](int n) { return n % 2 == 0; })
+      .then([&log](int n) { log("receive(even)", n); })
+      .finally([&log] { log("receive(even)", "close"); })
       .run_on(sch, 4);
 
-  sleep_for(1s);
+  std::this_thread::sleep_for(1s);
 
-  for (std::size_t i = 0; i < 100; ++i) {
+  for (std::size_t i = 0; i < 20; ++i) {
     odd.push(i);
     even.push(i);
   }
 
-  sleep_for(1s);
+  std::this_thread::sleep_for(1s);
 
   sch.halt();
   sch.wait();
