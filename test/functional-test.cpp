4d325165f7742c474e44aea526de68cd21a208a7
GobanTKG <yoshi.minase@gmail.com>
2018-08-14 00:23:30 +0900

    add test for bundle::bundle_with

--- test/functional-test.cpp
+++ test/functional-test.cpp
@@ -15,9 +15,22 @@ int main() {
       rat::thunk{}
           .filter([](auto n) { return n % 3 != 0 && n % 5 != 0; })
           .then([](auto n) { cout << n; }),
-      [](...) { cout << endl; }};
+      [](...) { cout << " "; }};
+
+  auto fizz_buzz_ = rat::bundle{}.bundle_with(
+      rat::thunk{}.filter([](auto n) { return n % 3 == 0; }).then([](...) {
+        cout << "Fizz";
+      }),
+      rat::thunk{}.filter([](auto n) { return n % 5 == 0; }).then([](...) {
+        cout << "Buzz";
+      }),
+      rat::thunk{}
+          .filter([](auto n) { return n % 3 != 0 && n % 5 != 0; })
+          .then([](auto n) { cout << n; }),
+      [](...) { cout << endl; });
 
   for (size_t i = 1; i <= 20; ++i) {
     fizz_buzz(i);
+    fizz_buzz_(i);
   }
 }
