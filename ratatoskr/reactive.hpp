54306947dbde8c0064feb70a59aa966de541a1b2
GobanTKG <yoshi.minase@gmail.com>
2018-08-14 07:20:05 +0900

    REWRITE signal.

--- ratatoskr/reactive.hpp
+++ ratatoskr/reactive.hpp
@@ -1,10 +1,10 @@
+#include "./concurrent.hpp"
+#include "./functional.hpp"
 #include <atomic>
 #include <forward_list>
 #include <memory>
-#include <ratatoskr/concurrent.hpp>
-#include <ratatoskr/functional.hpp>
+#include <stdexcept>
 #include <thread>
-#include <tuple>
 #include <utility>
 
 #ifndef RATATOSKR_REACTIVE_HPP
@@ -12,218 +12,122 @@
 
 namespace rat {
 inline namespace reactive {
-  template <class T>
-  class source;
 
-  template <class T, class Thunk, class... Finally>
+  template <class Source, class Thunk, class Finalizer>
   class signal;
 
-  template <class T>
-  class source {
-    rat::channel<T> ch;
+  template <class Source, class Th, class... Fins>
+  class signal<Source, rat::thunk<Th>, rat::bundle<Fins...>>
+      : public rat::functional::detail::composable<
+            signal<Source, rat::thunk<Th>, rat::bundle<Fins...>>> {
 
-  public:
-    source() {}
+    using Self = signal<Source, rat::thunk<Th>, rat::bundle<Fins...>>;
 
-    auto signal() { return rat::signal{ch, rat::thunk{}, std::tuple<>{}}; }
-
-    auto get_closer() { return ch.get_closer(); }
-
-    auto get_sender() { return ch.get_sender(); }
-
-    auto next() -> std::optional<T> {
-      if (!ch.is_closed()) {
-        static auto rc = ch.get_receiver();
-
-        try {
-          return std::optional{rc.next()};
-        }
-        catch (const rat::close_channel &) {
-          return std::nullopt;
-        }
-      }
-      else {
-        return std::nullopt;
-      }
-    }
-
-    template <class F>
-    [[nodiscard]] auto map(F &&f) {
-      return this->signal().map(std::forward<F>(f));
-    }
-
-    template <class F>
-    [[nodiscard]] auto try_map(F &&f) {
-      return this->signal().try_map(std::forward<F>(f));
-    }
-
-    template <class F>
-    [[nodiscard]] auto then(F &&f) {
-      return this->signal().then(std::forward<F>(f));
-    }
-
-    template <class F>
-    [[nodiscard]] auto filter(F &&f) {
-      return this->signal().filter(std::forward<F>(f));
-    }
-
-    template <class F>
-    [[nodiscard]] auto finally(F &&f) {
-      return this->signal.finally(std::forward<F>(f));
-    }
-
-    void push(const T &x) { ch.push(x); }
-    void push(T &&x) { ch.push(std::move(x)); }
-
-    void close() { ch.close(); }
-  };
-
-  template <class T, class Fs, class... Finally>
-  class signal {
-    template <class T_>
-    friend class source;
-
-    template <class T_, class Fs_, class... Finally_>
+    friend class rat::functional::detail::composable<Self>;
+    template <class Source_, class Thunk_, class Finalizer_>
     friend class signal;
 
-    rat::channel<T> ch;
-    rat::thunk<Fs> thunk;
-    std::tuple<Finally...> finalizer;
-    std::shared_ptr<std::atomic_bool> is_finalized_p;
-
-    template <size_t... I>
-    void call_finalizer(std::index_sequence<I...>) {
-      (std::get<I>(finalizer)(), ...);
+    template <class F>
+    [[nodiscard]] auto compose(F &&f) {
+      return rat::signal{std::move(receiver), std::move(closer),
+                         thunk.compose(f), finalizer};
     }
 
-    void finalize() {
-      if (*is_finalized_p)
-        return;
-      *is_finalized_p = true;
-      call_finalizer(std::make_index_sequence<sizeof...(Finally)>{});
-    }
+    rat::receiver<Source> receiver;
+    rat::channel_closer closer;
 
-    template <class Channel, class Thunk, class Tuple,
-              std::enable_if_t<
-                  (std::is_constructible_v<rat::channel<T>, Channel &&> &&
-                   std::is_constructible_v<rat::thunk<Fs>, Thunk &&> &&
-                   std::is_constructible_v<std::tuple<Finally...>, Tuple &&>),
-                  std::nullptr_t> = nullptr>
-    signal(Channel &&ch, Thunk &&thunk, Tuple &&finalizer)
-        : ch(std::forward<Channel>(ch)), thunk(std::forward<Thunk>(thunk)),
-          finalizer(std::forward<Tuple>(finalizer)),
-          is_finalized_p(std::make_shared<std::atomic_bool>(false)) {}
+    rat::thunk<Th> thunk;
+    rat::bundle<Fins...> finalizer;
+
+    signal(rat::receiver<Source> &&rc, rat::channel_closer &&cl,
+           const rat::thunk<Th> &th, const rat::bundle<Fins...> &fi)
+        : receiver(std::move(rc)), closer(std::move(cl)), thunk(th),
+          finalizer(fi) {}
 
   public:
-    using result_type = decltype(thunk(std::declval<T>()));
+    signal() = default;
+    signal(const Self &) = delete;
+    signal(Self &&) = default;
+    Self &operator=(const Self &) = delete;
+    signal(const rat::channel<Source> &ch)
+        : receiver(ch.get_receiver()), closer(ch.get_closer()) {}
+
+    template <class F>
+    [[nodiscard]] auto finally(F f) {
+      return rat::signal{std::move(receiver), std::move(closer), thunk,
+                         finalizer.bundle_with(f)};
+    }
 
     void run() {
-      if (!ch.is_closed()) {
-        static auto rc = ch.get_receiver();
-
-        try {
-          while (true) {
-            thunk(rc.next());
-          }
-        }
-        catch (const rat::close_channel &) {
-          finalize();
-        }
+      if (!receiver.valid()) {
+        throw std::logic_error{"Running signal that has been invalid."};
       }
-      else {
-        finalize();
+      if (receiver.is_closed()) {
+        finalizer();
+        return;
       }
-    }
 
-    auto next() -> std::optional<result_type> {
-      if (!ch.is_closed()) {
-        static auto rc = ch.get_receiver();
-
-        try {
-          return std::optional{thunk(rc.next())};
-        }
-        catch (const rat::close_channel &) {
-          finalize();
-          return std::nullopt;
+      try {
+        while (true) {
+          thunk(receiver.next());
         }
       }
-      else {
-        finalize();
-        return std::nullopt;
+      catch (const rat::close_channel &) {
+        finalizer();
       }
-    }
-
-    template <class F>
-    [[nodiscard]] auto map(F &&f) & {
-      return rat::signal{ch, thunk.map(std::forward<F>(f)), finalizer};
-    }
-    template <class F>
-    [[nodiscard]] auto map(F &&f) && {
-      return rat::signal{std::move(ch), thunk.map(std::forward<F>(f)),
-                         std::move(finalizer)};
-    }
-
-    template <class F>
-    [[nodiscard]] auto try_map(F &&f) & {
-      return rat::signal{ch, thunk.try_map(std::forward<F>(f)), finalizer};
-    }
-    template <class F>
-    [[nodiscard]] auto try_map(F &&f) && {
-      return rat::signal{std::move(ch), thunk.try_map(std::forward<F>(f)),
-                         std::move(finalizer)};
-    }
-
-    template <class F>
-    [[nodiscard]] auto then(F &&f) & {
-      return rat::signal{ch, thunk.then(std::forward<F>(f)), finalizer};
-    }
-    template <class F>
-    [[nodiscard]] auto then(F &&f) && {
-      return rat::signal{std::move(ch), thunk.then(std::forward<F>(f)),
-                         std::move(finalizer)};
-    }
-
-    template <class F>
-    [[nodiscard]] auto filter(F &&f) & {
-      return rat::signal{ch, thunk.filter(std::forward<F>(f)), finalizer};
-    }
-    template <class F>
-    [[nodiscard]] auto filter(F &&f) && {
-      return rat::signal{std::move(ch), thunk.filter(std::forward<F>(f)),
-                         std::move(finalizer)};
-    }
-
-    template <class F>
-    [[nodiscard]] auto finally(F &&f) & {
-      return rat::signal{
-          ch, thunk, std::tuple_cat(finalizer, std::tuple{std::forward<F>(f)})};
-    }
-    template <class F>
-    [[nodiscard]] auto finally(F &&f) && {
-      return rat::signal{
-          std::move(ch), std::move(thunk),
-          std::tuple_cat(finalizer, std::tuple{std::forward<F>(f)})};
     }
 
     void run_on(rat::scheduler &sch) {
-      sch.connect(std::thread([*this]() mutable { this->run(); }),
-                  ch.get_closer());
+      sch.connect(
+          std::thread([self = std::move(*this)]() mutable { self.run(); }),
+          closer);
     }
 
-    void run_on(rat::scheduler &sch, std::size_t n) {
-      std::forward_list<std::thread> ths;
-      for (std::size_t i = 0; i < n; ++i) {
-        ths.emplace_front([*this]() mutable { this->run(); });
+    void run_on(rat::scheduler &sch, std::size_t parallelism) {
+      if (!receiver.valid()) {
+        throw std::logic_error{"Running signal that has been invalid."};
       }
-      sch.connect(ths, ch.get_closer());
+      if (receiver.is_closed()) {
+        sch.connect(std::thread(
+            [finalizer = this->finalizer]() mutable { finalizer(); }));
+        return;
+      }
+
+      auto shared_receiver = receiver.share();
+      auto is_finalized = std::make_shared<std::atomic_bool>(false);
+      std::forward_list<std::thread> ths;
+
+      for (std::size_t i = 0; i < parallelism; ++i) {
+        ths.push_front(
+            std::thread([thunk = this->thunk, finalizer = this->finalizer,
+                         shared_receiver, is_finalized]() mutable {
+              try {
+                while (true) {
+                  thunk(shared_receiver->next());
+                }
+              }
+              catch (const rat::close_channel &) {
+                if (*is_finalized) {
+                  return;
+                }
+                *is_finalized = true;
+                finalizer();
+              }
+            }));
+      }
+
+      sch.connect(ths, closer);
     }
   };
 
-  template <class T, class Fs, class... Finally>
-  signal(rat::channel<T>, rat::thunk<Fs>, std::tuple<Finally...>)
-      ->signal<T, Fs, Finally...>;
+  template <class Source>
+  signal(rat::channel<Source>)->signal<Source, rat::thunk<void>, rat::bundle<>>;
+
+  template <class Source, class Th, class... Fins>
+  signal(rat::receiver<Source>, rat::channel_closer, rat::thunk<Th>,
+         rat::bundle<Fins...>)
+      ->signal<Source, rat::thunk<Th>, rat::bundle<Fins...>>;
 
 } // namespace reactive
 } // namespace rat
-
 #endif
